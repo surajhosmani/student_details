612a330d1213490f56a7ccaffb510fffb98e02a4
surajhosmani <surajhosmani83@gmail.com>
2025-11-20 16:32:02 +0530

    Create details.py

---
+++ details.py
@@ -0,0 +1,14 @@
+import sys
+if len(sys.argv) == 3:
+  script_name = sys.argv[0]
+  name = sys.argv[1]
+  rollno = sys.argv[2]
+  print("User provided input values:")
+else:
+  script name = sys_argv[0]
+  name == "Suraj"
+  rollno == "151"
+  print("No input given == using default values:")
+print("Script Name:", script_name)
+print("Student Name:",name)
+print("Roll Number:",rollno)
